53c628ab17cda00e7123758e6fa3cadf7cd3ab5f
Ramdas <99587290+JiteshK1@users.noreply.github.com>
2024-02-12 00:02:38 +0530

    Tree Questions

---
+++ src/Strings/stringPrac/NonRepeating.java
@@ -0,0 +1,25 @@
+package Strings.stringPrac;
+import  java.util.*;
+public class NonRepeating {
+    public static void main(String[] args) {
+
+        System.out.println(
+                firstUniqChar("leetcode")
+        );
+    }
+    public static int firstUniqChar(String s) {
+        HashMap<Character, Integer> mp = new HashMap<>();
+
+        for (char a : s.toCharArray()) {
+            mp.put(a, mp.getOrDefault(a, 0) + 1);
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (mp.get(s.charAt(i)) == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
